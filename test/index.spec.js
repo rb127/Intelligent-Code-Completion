2fcfa53e8d99ccb9ead66f7b270aef3d5842ae7a
ShahmirU <shahmirusman1999@gmail.com>
2021-11-07 00:15:57 -0400

    Started acceptance test suite

--- test/index.spec.js
+++ test/index.spec.js
@@ -9,3 +9,34 @@ describe('helloWorld', () => {
         expect(result.body[0].declarations[0].init.value).to.equal("Hello World")
     });
 });
+
+describe('Acceptance tests', () => {
+    it('should return empty list if file is empty', () => {
+        console.log("Running Test")
+        const result = returnSuggestions(` ^`);
+        expect(result).to.equal([])
+    });
+
+    it('should return empty list if no cursor found', () => {
+        console.log("Running Test")
+        const result = returnSuggestions(`const foo = bar;`);
+        expect(result).to.equal([])
+    });
+
+    it('should return empty list if no character before cursor', () => {
+        console.log("Running Test")
+        const result = returnSuggestions(
+            `const foo = bar;
+             const a = fo ^
+        `);
+        expect(result).to.equal([])
+    });
+});
+
+describe('Unit tests', () => {
+    it('should return empty list if no cursor found', () => {
+        console.log("Running Test")
+        const result = returnSuggestions(``);
+        expect(result).to.equal([])
+    });
+});
