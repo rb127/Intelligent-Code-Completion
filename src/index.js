b7382064b7be9c657ac5f3b7d3b255d09c295e03
Chinmaya Nathany <chnimaya.nathany@gmail.com>
2021-11-08 04:26:22 -0500

    Add blueprint for runner code (preliminary)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { parse } from 'acorn-loose';
-import { readFileSync } from 'fs';
-import { simple, full, ancestor } from 'acorn-walk';
+import { readFileSync, writeFileSync } from 'fs';
+import { simple, full, ancestor, fullAncestor, findNodeAt, findNodeAround, findNodeAfter } from 'acorn-walk';
 
 const readInputFile = () => {
     try {
@@ -17,15 +17,69 @@ export const customParse = () => {
 }
 
 const parsedData = customParse();
+console.log(JSON.stringify(parsedData, 0, 2))
+writeFileSync("parsedData.json", JSON.stringify(parsedData, 0, 2))
 
-simple(parsedData, {
-    Literal(node) {
-        console.log(`Found a literal: ${node.value}`)
+
+let parsedNode = findNodeAt(parsedData, 64, 65, 'VariableDeclarator')
+console.log(parsedNode)
+
+full(parsedData, node => {
+    // console.log(`There's a ${node.type} node at ${node.ch}`)
+
+    if(node.type == 'Program'){
+        writeFileSync("fullWalk.json", JSON.stringify(node, 0, 2))
+        parsedNode = node
     }
-})
+    // fullAncestor(node)
+  })
+
+let nodeToPrint = ''
+  fullAncestor(parsedData, (node, state, ancestors) => {
+    // console.log(`There's a ${node.type} node at ${node.ch}`)
+    // if(node.type == 'Program'){
+
+    // }
+    // const nodeToPrint = node.type == 'Program'? JSON.stringify(node, 0, 2) : ''
+    // if(node.type == 'Program'){
+    //     writeFileSync("data.json", JSON.stringify(node, 0, 2))
+    //     parsedNode = node
+    // }
+    // fullAncestor(node)
+    if(node.start > 63 && node.end < 66)
+    {
+        console.log('saved')
+        console.log(ancestors)
+    }
+  })
+//   console.log(nodeToPrint)
+
+// ancestor(parsedData, {
+//     'VariableDeclaration': function (node, parents) {
+//       var parent = null;
+//       for (var i = parents.length - 1; i >= 0 && parent === null; i--) {
+//         if (node.kind === 'var' ? isScope(parents[i]) : isBlockScope(parents[i])) {
+//           parent = parents[i];
+//         }
+//       }
+//       parent.locals = parent.locals || {};
+//       node.declarations.forEach(function (declaration) {
+//         declarePattern(declaration.id, parent);
+//       });
+//     },
+// })
 
 ancestor(parsedData, {
     Literal(_, ancestors) {
-        console.log("This literal's ancestors are:", ancestors.map(n => n.type))
+        // console.log("This literal's ancestors are:", ancestors.map(n => n.type))
+        writeFileSync("ancestors.json", JSON.stringify(ancestors.map(n => n.type), 0, 2))
     }
 })
+
+// console.log(JSON.stringify(findNodeAround(parsedData, 61), 0 ,2))
+// writeFileSync("fullWalk.json", JSON.stringify(node, 0, 2))
+
+
+const cursorPos = 69;
+let node = findNodeAround(parsedData, cursorPos - 1)
+writeFileSync("currentCursorNode.json", JSON.stringify(node, 0, 2))
