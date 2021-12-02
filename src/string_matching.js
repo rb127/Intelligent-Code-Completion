b3aecc88a6b0b899535daaa144391dfe5e72e033
Chinmaya Nathany <chinmaya.nathany@gmail.com>
2021-12-02 04:55:33 -0500

    Fuzzy Search Implementation for Searching Lists tolerantly

---
+++ src/string_matching.js
@@ -0,0 +1,34 @@
+import Fuse from "fuse.js";
+
+export function stringMatch(words, searchStr, threshold_score = 0.3, orderedByScope = false) {
+    /*
+    Perform fuzzy  string matching using the Fuse library
+    Params:
+        words - A list of words to be searched (our universe)
+        searchStr - The Search phrase to search for
+        theresholdScore - The error tolerance level (a lesser number is less error tolerant)
+    */
+    
+    let options = {
+        includeScore: true,
+        minMatchCharLenth: 2,
+        shouldSort: !orderedByScope,
+        location: 0,
+        threshold: threshold_score}
+    
+    const fuse = new Fuse(words, options)
+    const result = fuse.search(searchStr)
+    
+    let suggestions = []
+    result.forEach(element => {
+      if(element.score <= threshold_score){
+          suggestions.push(element.item)
+      }
+    });
+
+    return suggestions
+}
+
+let words = ['cursor', 'parsor', 'parser']
+let searchStr = 'carsor'
+console.log(stringMatch(words, searchStr))
