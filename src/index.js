d7a1bf52af193937a5fbe2057a3f1f08cf0a7075
ShahmirU <shahmirusman1999@gmail.com>
2021-12-02 03:08:39 -0500

    Moved core functionality to getReferenceString func

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import { readFileSync, writeFileSync } from 'fs';
 import { simple, full, ancestor, fullAncestor, findNodeAt, findNodeAround, findNodeAfter } from 'acorn-walk';
 
 const CURSOR_SYMBOL = '^'
-const masterList = []
 /* 
 fileContents: Input file read as String
 Returns cursor position as denoted by CURSOR_SYMBOL if present
@@ -26,11 +25,6 @@ export const parseInputFile = (fileContents) => {
     return parsedResult
 }
 
-const parsedData = parseInputFile(readInputFile());
-writeFileSync("parsedData.json", JSON.stringify(parsedData, 0, 2))
-
-let parsedNode = findNodeAt(parsedData, 64, 65, 'VariableDeclarator')
-// console.log(parsedNode)
 
 /*
 Returns the word just before the cursor.
@@ -47,62 +41,19 @@ export const getReferenceString = (fileContents, cursorPos) => {
     }
 }
 
-full(parsedData, node => {
-    // console.log(`There's a ${node.type} node at ${node.ch}`)
-
-    if (node.type == 'Program') {
-        writeFileSync("fullWalk.json", JSON.stringify(node, 0, 2))
-        parsedNode = node
-    }
-})
-
-fullAncestor(parsedData, node => {
-    // console.log(`There's a ${node.type} node at ${node.ch}`)
-
-    if (node.type == 'Program') {
-        writeFileSync("fullAncestor.json", JSON.stringify(node, 0, 2))
-        parsedNode = node
-    }
-})
-
-// Template code for future use
-// let nodeToPrint = ''
-//   fullAncestor(parsedData, (node, state, ancestors) => {
-//     // console.log(`There's a ${node.type} node at ${node.ch}`)
-//     // if(node.type == 'Program'){
-
-//     // }
-//     // const nodeToPrint = node.type == 'Program'? JSON.stringify(node, 0, 2) : ''
-//     // if(node.type == 'Program'){
-//     //     writeFileSync("data.json", JSON.stringify(node, 0, 2))
-//     //     parsedNode = node
-//     // }
-//     // fullAncestor(node)
-//     if(node.start > 63 && node.end < 66)
-//     {
-//         console.log('saved')
-//         console.log(ancestors)
-//     }
-//   })
-
-// ancestor(parsedData, {
-//     Literal(_, ancestors) {
-//         // console.log("This literal's ancestors are:", ancestors.map(n => n.type))
-//         writeFileSync("ancestors.json", ancestors.map(n => n.type))
-//     }
-// })
-
 // How to find cursor node given a cursor position
-const getCursorNode = (cursorPos) => {
+const getCursorNode = (parsedData,cursorPos) => {
     let cursorNode = findNodeAround(parsedData, cursorPos - 1)
     writeFileSync("currentCursorNode.json", JSON.stringify(cursorNode, 0, 2))
     return cursorNode
 }
 
-export const returnSuggestions = () => {
+export const returnSuggestions = (file) => {
     //TODO
+    console.log("FILE", file)
     // check validity of input
-    const file = readInputFile()
+    let masterList = []
+    // const file = readInputFile()
     const cursorPosition = getCursorPosition(file)
     // no caret symbol found 
     if (cursorPosition === -1) {
@@ -111,33 +62,33 @@ export const returnSuggestions = () => {
     }
 
     console.log("Cursor position is at", cursorPosition)
-    console.log("Reference string is", getReferenceString(file, cursorPosition))
+    const referenceString = getReferenceString(file, cursorPosition)
+    console.log("Reference string is", referenceString)
+    if (referenceString == ""){
+        console.log("whitespace found before cursor \nSuggestions: []")
+        return []
+    }
 
-    const cursorNode = getCursorNode(cursorPosition)
+    const parsedData = parseInputFile(file);
+
+    const cursorNode = getCursorNode(parsedData, cursorPosition)
+
     const nodeTypeFn = new Function(cursorNode.node.type)
     console.log(nodeTypeFn)
     console.log(cursorNode.node.start)
-    // ancestor(parsedData, {
-    //     Literal(_, ancestors) {
-    //         // console.log("This literal's ancestors are:", ancestors.map(n => n.type))
-    //         writeFileSync("ancestors.json", JSON.stringify(ancestors.map(n => n.type), 0, 2))
-    //     },
-    //     // Identifier(node, ancestors) {
-    //     //     //console.log("Found identifier")
-    //     //     // console.log("This literal's ancestors are:", ancestors.map(n => n.type))
-    //     //     if (node.start == cursorNode.node.start && node.end == cursorNode.node.end) {
-    //     //         console.log("Found Cursor Node")
-    //     //         writeFileSync("ancestors.json", JSON.stringify(ancestors.map(n => n.type), 0, 2))
-    //     //     }
-    //     // }
-    // })
+
+    writeFileSync("parsedData.json", JSON.stringify(parsedData, 0, 2))
+    
     fullAncestor(parsedData, (node, err, ancestors) => {
         // console.log(`There's a ${node.type} node at ${node.ch}`)
 
         if (node.type == cursorNode.node.type && node.start == cursorNode.node.start && node.end == cursorNode.node.end) {
             console.log("Found Cursor node")
             // console.log(ancestors)
-            for (const ancestor of ancestors) {
+            for (let i = ancestors.length - 1; i >= 0; i--) {
+                const ancestor = ancestors[i]
+            // }            
+            // for (const ancestor of ancestors) {
                 if ("body" in ancestor){
                     // console.log("BODY", ancestor.body)                    
                     for (const subNode of ancestor.body){
@@ -152,10 +103,12 @@ export const returnSuggestions = () => {
             }
             console.log("MASTER", masterList)
             writeFileSync("fullAncestor.json", JSON.stringify(node))
-            parsedNode = node
         }
     })
+    return masterList
 
 }
 
-returnSuggestions()
+const result = returnSuggestions(readInputFile())
+
+console.log(result)
